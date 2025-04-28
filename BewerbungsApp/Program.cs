dfb66f2a935073e72ea60a8d67030394f1e6c070
Sin-Mini <Sin-Mini@SIN-MINI>
2025-04-28 19:08:20 +0200

    Projektdateien hinzufügen.

---
+++ BewerbungsApp/Program.cs
@@ -0,0 +1,324 @@
+﻿namespace BewerbungsApp
+{
+
+    internal class Program
+    {
+        private static int pos = 0;
+        private static int language = 0;
+        private static List<string> langItems = [];
+        private static int SetLanguage(int input) {
+            langItems.Clear();
+            langItems = [];
+            switch (input)
+            {
+                case 0:
+                    // Grundlegende Sachen
+                    langItems.Add("Eingabe: ");                                                         // 0
+                    langItems.Add("Err: Keine Eingabe");                                                // 1
+                    langItems.Add("Err: Ungültige Eingabe");                                            // 2
+                    langItems.Add("Anwendung wird beendet");                                            // 3
+                    langItems.Add("Sie haben als Sprache Deutsch ausgewählt.");                         // 4
+
+                    // Alle Funktionen
+                    langItems.Add("Folgende Optionen stehen zur Verfügung:");                           // 5
+                    langItems.Add("0 | Datenbank | SQL / XML");                                         // 6
+                    langItems.Add("1 | Benutzeroberfläche | Java");                                     // 7
+                    langItems.Add("2 | Kommunikation | PHP");                                           // 8
+                    langItems.Add("3 | ... | JavaScript");                                              // 9
+
+                    // Datenbank Optionen
+                    langItems.Add("Folgende Datenbank Optionen stehen zur Verfügung:");                 // 10
+                    langItems.Add("0 | Erstelle eine Datenbank");                                       // 11
+                    langItems.Add("1 | Lade 'Hard-Coded' Datenbank");                                   // 12
+                    langItems.Add("2 | Importiere eine Datenbank");                                     // 13
+                    langItems.Add("3 | Exportiere the Datenbank");                                      // 14
+
+                    // Liste mit allen Befehlen
+                    langItems.Add(" * Liste aller Befehle:");                                           // 
+                    langItems.Add(" * Sprache -WERT | Um die Sprache zu ändern");                       // 
+                    langItems.Add(" * Menu          | Öffnet das Hauptfenster mit allen Funktionen");   // 
+                    langItems.Add(" * Exit          | Beendet die Anwendung");                          // 
+                    break;
+
+                case 1:
+                    // Basic stuff
+                    langItems.Add("Input: ");                                                   // 0
+                    langItems.Add("Err: No input");                                             // 1
+                    langItems.Add("Err: Invalid input");                                        // 2
+                    langItems.Add("Closing the application");                                   // 3
+                    langItems.Add("You've selected english as your language.");                 // 4
+
+                    // All functions
+                    langItems.Add("Following options are available:");                          // 5
+                    langItems.Add("0 | Database | SQL / XML");                                  // 6
+                    langItems.Add("1 | User Interface | Java");                                 // 7
+                    langItems.Add("2 | Communication | PHP");                                   // 8
+                    langItems.Add("3 | ... | JavaScript");                                      // 9
+
+                    // Database options
+                    langItems.Add("Following database options are available:");                 // 10
+                    langItems.Add("0 | Create a database");                                     // 11
+                    langItems.Add("1 | Load 'Hard coded' database");                            // 12
+                    langItems.Add("2 | Import a database");                                     // 13
+                    langItems.Add("3 | Export the database");                                   // 14
+
+                    // List of all commands
+                    langItems.Add(" * List of all commands:");                                  // 
+                    langItems.Add(" * Lang -VALUE   | To change the language");                 // 
+                    langItems.Add(" * Menu          | Opens the menu with all functions");      // 
+                    langItems.Add(" * Exit          | Closes the application");                 // 
+                    break;
+            }
+
+            return language = input; 
+        }
+
+        static void Main()
+        {
+            PostMessage(-1);
+            Help();
+
+            PostMessage(0);
+            
+            InitializeApp();
+        }
+        private static void InitializeApp ()
+        {
+            pos = 0;
+            Console.Write("Eingabe / Input: ");
+            string input = CheckInput();
+
+            if (input == "0" || input.Equals("deutsch", StringComparison.CurrentCultureIgnoreCase))
+            {
+                SetLanguage(0);
+                pos = 1;
+
+                Console.WriteLine(langItems[4] + "\n");
+
+                PostMessage(1);
+                ShowAllFunctions();
+            }
+            else if (input == "1" || input.Equals("english", StringComparison.CurrentCultureIgnoreCase))
+            {
+                SetLanguage(1);
+                pos = 1;
+
+                Console.WriteLine(langItems[4] + "\n");
+
+                PostMessage(1);
+                ShowAllFunctions();
+            }
+            else if (input != "0" || input != "1" || !input.Equals("deutsch", StringComparison.CurrentCultureIgnoreCase) || !input.Equals("english", StringComparison.CurrentCultureIgnoreCase))
+            {
+                InitializeApp();
+            }
+        }
+        private static void ShowAllFunctions()
+        {
+            Console.Write(langItems[0]);
+            string input = CheckInput();
+
+            if (input == "0")
+            {
+                PostMessage(2);
+                Database();
+            }
+            else if (input == "1")
+            {
+                GUI();
+            }
+            else if (input == "2")
+            {
+                Communication();
+            }
+            else if (input == "3")
+            {
+
+            }
+            else
+            {
+                ShowAllFunctions();
+            }
+        }
+
+        private static void Database ()
+        {
+            Console.Write(langItems[0]);
+            string input = CheckInput();
+
+            if (input == "____")
+            {
+
+            }
+            else
+            {
+                Database();
+            }
+        }
+
+        private static void GUI ()
+        {
+            Console.Write(langItems[0]);
+            string input = CheckInput();
+
+        }
+
+        private static void Communication ()
+        {
+            Console.Write(langItems[0]);
+            string input = CheckInput();
+        }
+
+        private static string CheckInput ()
+        {
+            string? checkInput = Console.ReadLine();
+
+            if (pos == 0)
+            {
+                switch (checkInput?.ToUpper())
+                {
+                    case "":
+                        Console.WriteLine("Err: Keine Eingabe / No input\n");
+                        break;
+
+                    case "HELP" or "HILFE":
+                        Help();
+                        break;
+
+                    case "EXIT":
+                        Console.WriteLine("Anwendung wird beendet / Closing the application");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            else
+            {
+                switch (checkInput?.ToUpper())
+                {
+                    case "":
+                        Console.WriteLine(langItems[1] + "\n");
+                        break;
+
+                    case string when 
+                    (checkInput.ToUpper().Contains("SPRACHE ") || checkInput.ToUpper().Contains("LANG ")) && 
+                    (checkInput.Contains("-0") || checkInput.ToUpper().Contains("-DEUTSCH") || checkInput.Contains("-1") || 
+                    checkInput.ToUpper().Contains("-ENGLISH")):
+                        string[] getLang = checkInput.Split('-');
+                        if (getLang.Length > 2)
+                        {
+                            Console.WriteLine(langItems[2] + "\n");
+                            break;
+                        }
+
+                        if (getLang[1].Length <= 1)
+                        {
+                            SetLanguage(int.Parse(getLang[1]));
+                            Console.WriteLine(langItems[4] + "\n");
+                        } 
+                        else
+                        {
+                            getLang[1].PadRight(getLang[1].Length - 1);
+                            Console.WriteLine($"waiting for stuff {getLang[1]}");
+                        }
+                        break;
+
+                    case "HELP" or "HILFE":
+                        Help();
+                        break;
+
+                    case "MENU":
+                        pos = 1;
+                        PostMessage(1);
+                        ShowAllFunctions();
+                        break;
+
+                    case "EXIT":
+                        Console.WriteLine(langItems[3]);
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+            return checkInput;
+        }
+
+        private static void Help()
+        {
+            string helpMsg = "\n/******************** Hilfe / Help ********************\n";
+            if (pos == 0)
+            {
+                helpMsg +=
+                " * \n" +
+                " * Liste aller Befehle:\n" +
+                " * Sprache -WERT | Sprache ändern\n" +
+                " * Menu          | Öffnet das Hauptfenster mit allen Funktionen\n" +
+                " * Exit          | Beendet die Anwendung\n" +
+                " * \n" +
+                " * List of all commands:\n" +
+                " * Lang -VALUE   | Change the language\n" +
+                " * Menu          | Opens the menu with all functions\n" +
+                " * Exit          | Closes the application\n" +
+                " * ";
+            }
+            else
+            {
+                helpMsg += " * \n";
+                    for (int i = 0; i < 4; i++)
+                {
+                    helpMsg += langItems[langItems.Count - 4 + i] + "\n";
+                }
+                helpMsg += " * ";
+            }
+            helpMsg += "\n ********************* Ende / End *********************/\n";
+
+            Console.WriteLine(helpMsg);
+        }
+
+        private static void PostMessage (int value)
+        {
+            switch (value)
+            {
+                case -2:
+                    Console.WriteLine("\n/******************************************************/\n");
+                    break;
+
+                case -1:
+                    Console.WriteLine(
+                    "/******************************************************\n" +
+                    " *                                                    *\n" +
+                    " *       Diese APP wurde von Melina Kotschetkow       *\n" +
+                    " *          für Bewerbungszwecke geschrieben          *\n" +
+                    " *                                                    *\n" +
+                    " ******************************************************/\n" +
+                    "");
+                    break;
+
+                case 0:
+                    Console.WriteLine(
+                        "   Wähle deine Sprache      |   Select your language\n" +
+                        "   Deutsch: 0               |   English: 1\n"
+                        ); 
+                    break;
+
+                case 1:
+                    Console.WriteLine("/******************************************************/\n");
+                    for (int i = 0; i <= 4; i++)
+                    {
+                        Console.WriteLine(langItems[5 + i]);
+                    }
+                    Console.Write(Environment.NewLine);
+                    break;
+
+                case 2:
+                    Console.WriteLine("\n/******************************************************/\n");
+                    for (int i = 0; i <= 4; i++)
+                    {
+                        Console.WriteLine(langItems[10 + i]);
+                    }
+                    Console.Write(Environment.NewLine);
+                    break;
+            }
+        }
+
+    }
+}
